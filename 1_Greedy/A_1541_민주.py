42544af68e4ca0993f471e5aa9b6e40ee0a284df
minjuko <88755369+minjuuko@users.noreply.github.com>
2023-01-06 03:47:16 +0900

    민주 1_A 제출

--- 1_Greedy/A_1541_민주.py
+++ 1_Greedy/A_1541_민주.py
@@ -1,12 +1,16 @@
 # 1541 잃어버린 괄호
 # 최솟값을 만드려면, - 부호를 기준으로 괄호 치기
+# - 부호 뒤 값들이 커져야 결과값이 최소가 됨
 # - 부호를 기준으로  분할, - 부호 등장 전 수들은 + 부호 기준으로 분할 후 더하여 result에 저장
 # for문) + 부호를 기준으로 분할하여 수를 더하고, 더한 값을 result에서 뺄셈
 
-x = (input().split('-'))   # -를 기준으로 분할, +로만 구성된 식
+x = (input().split('-'))
+# x : 수식을 입력받아 -를 기준으로 분할하여 배열에 저장 -> 숫자와 +로만 구성
 
-result = sum(map(int, x[0].split('+')))   # +를 기준으로 분할하여 덧셈 계산 -> result에 저장
+result = sum(map(int, x[0].split('+')))
+# -부호 이전 수들은 +를 기준으로 분할하여 덧셈 계산 -> result에 저장
 
-for i in range(1, len(x)):  # 반복문 1부터 끝까지
-	result -= sum(map(int, x[i].split('+')))  # +부호를 기준으로 분할하여 수를 더한 값을 result에서 빼기 
-print(result)   # 최솟값 출력 
+for i in range(1, len(x)):   # 반복문
+	result -= sum(map(int, x[i].split('+')))        
+# 그 이외의 수들은 +부호를 기준으로 분할하여 수를 모두 더한 값을 result에서 빼기
+print(result)   # 최솟값 출력
