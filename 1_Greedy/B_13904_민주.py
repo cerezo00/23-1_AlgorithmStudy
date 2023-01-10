62d90d1928073e0197eb33001bfd9fefc525bd35
minjuko <88755369+minjuuko@users.noreply.github.com>
2023-01-10 13:47:32 +0900

    민주 1_B

--- 1_Greedy/B_13904_민주.py
+++ 1_Greedy/B_13904_민주.py
@@ -1,27 +1,25 @@
 # 13904 과제
-# 메모리 30616 KB, 시간 36ms
+# 메모리 30616 KB, 시간 40ms
 # 과제 마감일, 과제 점수 -> 받을 수 있는 점수의 최댓값
-
+# 점수 내림차순 정렬 -> 높은 점수는 최대한 기한 마지막날에 수행
+# 마감일에 다른 처리해야할 과제가 있다면 그 이전 날 중 수행
 import sys
-N = int(sys.stdin.readline())  # 정수 N 입력
-homework = []  # 과제 마감일 점수 저장
-visit = [False] * 1001  # 과제 완료 여부 확인
+input = sys.stdin.readline
 
-for _ in range(N):
-    d, w = map(int, sys.stdin.readline().split())   # d:과제 마감일까지 남은 일수, w: 과제의 점수
-    homework.append((d, w))     # d,w를 homework에 저장
-
-homework.sort(key=lambda x: x[1], reverse=True)  # 내림차순 정렬
+N = int(input())  # 정수 N 입력
+arr = []  # 과제 마감일 점수 저장할 배열
 result = 0   # 받을 수 있는 점수의 최댓값 저장할 변수
+check = [0 for i in range(1001)]  # 과제 처리 완료 여부 확인
 
-for date, score in homework:   # homework에서 탐색
-    i = date
-    while i > 0 and visit[i]: # 과제를 할 날짜 탐색
-        i -= 1
-    if i == 0:         # 과제할 날짜가 없으면
-        continue      # 넘기기
-    else:             # 과제할 날짜가 있으면
-        visit[i] = True   # True로 저장
+for i in range(N):
+    d, w = map(int, input().split())   # d:과제 마감일까지 남은 일수, w: 과제의 점수
+    arr.append((d, w))     # d,w를 homework에 저장
+arr.sort(reverse=True, key=lambda x: x[1])  # 내림차순 정렬
+
+for date, score in arr:   # arr에서 탐색
+    while date > 0 and check[date]!=0: # 과제를 할 날짜 탐색
+        date -= 1          # 그 이전 날짜로
+    if date != 0:         # 가능한 과제가 있다면
+        check[date] = True   # 날짜 i에 처리하는 것으로 저장
         result += score   # result에 score값 더함
-
 print(result)  # 최댓값 출력
