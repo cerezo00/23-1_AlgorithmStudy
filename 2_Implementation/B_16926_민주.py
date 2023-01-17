0aaa558c5776169fc5992d719414e2fe3420fec5
minjuko <88755369+minjuuko@users.noreply.github.com>
2023-01-17 14:18:40 +0900

    민주 2_B

--- 2_Implementation/B_16926_민주.py
+++ 2_Implementation/B_16926_민주.py
@@ -1,5 +1,5 @@
 # 16926 배열 돌리기 1
-# 메모리 116884KB, 시간 560ms (pypy3)
+# 메모리 116912KB, 시간 552ms
 # 배열 R번 회전 (반시계)
 import sys
 input = sys.stdin.readline
@@ -11,37 +11,37 @@ for _ in range(N):
     A.append(list(map(int, input().split())))    # 배열 A 원소 입력
 
 # 바깥쪽 사각형 -> 안쪽 사각형
-def rotate():  # 배열 회전 
+def rotate():
     for k in range(min(N, M) // 2):  # 범위 : 사각형 수
         x, y = k, k
         tmp = A[x][y]  # 임시로 좌표 저장할 배열 생성 (초기값)
 
-        for j in range(k + 1, N - k):  # 왼쪽 (X값 증가, Y 고정)
+        for j in range(k + 1, N - k):  # 왼쪽 (X값 +, Y 고정)
             x = j  # x 좌표가 j이면
             former = A[x][y]  # 이전 좌표를 former에 저장
             A[x][y] = tmp  # A에 바꾼 좌표 저장
             tmp = former  # 바꾼 좌표를 이전 좌표에 저장
 
-        for j in range(k + 1, M - k):  # 아래쪽 (X값 고정, Y값 증가)
+        for j in range(k + 1, M - k):  # 아래쪽 (X값 고정, Y값 +)
             y = j
             former = A[x][y]
             A[x][y] = tmp
             tmp = former
 
-        for j in range(k + 1, N - k):  # 오른쪽 (X값 감소, Y값 고정)
+        for j in range(k + 1, N - k):  # 오른쪽 (X값 -, Y값 고정)
             x = N - j - 1
             former = A[x][y]
             A[x][y] = tmp
             tmp = former
 
-        for j in range(k + 1, M - k):  # 위쪽 (X값 고정, Y값 감소)
+        for j in range(k + 1, M - k):  # 위쪽 (X값 고정, Y값 -)
             y = M - j - 1
             former = A[x][y]
             A[x][y] = tmp
             tmp = former
 
-for _ in range(R):    # R번 회전 
-    rotate()
+for _ in range(R):
+    rotate()          # R번 회전하기
 
 for i in range(N):
     for j in range(M):
