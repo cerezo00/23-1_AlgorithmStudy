926f3b59458b5a063b22e14d4630056bbd892b15
Byeol Bam <cerezo00@naver.com>
2023-02-13 17:03:17 +0900

    .
    .

--- 4_Sorting/B_1744_남수민.py
+++ 4_Sorting/B_1744_남수민.py
@@ -12,36 +12,26 @@ result = 0
 for i in range(size):
     a = int(input())
 
-    # 양수 집합 입력
-    # 단, 1은 예외처리 : (1 * a) < (1 + a)
-    if a > 0:
-        if a == 1:
+    if a > 0:  # 양수 집합
+        if a == 1:  # 단, 1은 예외처리 : (1 * a) < (1 + a)
             result += 1
         else:
             array[0].append(a)
-    # 음수 집합 입력
-    else:
+
+    else:  # 음수 집합
         array[1].append(a)
 
-array[0].sort()
-array[1].sort()
+array[0].sort(reverse=True)  # 양수 : 내림차순
+array[1].sort()              # 음수 : 오름차순
 
-# 양수 집합 계산
-pn_idx = len(array[0]) - 2
-while pn_idx >= 0:
-    result += array[0][pn_idx+1] * array[0][pn_idx]
-    pn_idx -= 2
-# 홀수개 일 경우 남은 하나 처리
-if pn_idx == -1:
-    result += array[0][0]
-
-# 음수, 0 집합 계산
-nn_idx = 1
-while nn_idx < len(array[1]):
-    result += array[1][nn_idx-1] * array[1][nn_idx]
-    nn_idx += 2
-# 홀수개 일 경우 남은 하나 처리
-if nn_idx == len(array[1]):
-    result += array[1][-1]
+# 두 개씩 곱하여 합산
+for i in range(2):
+    index = 1
+    while index <= len(array[i]):
+        result += array[i][index-1] * array[i][index]
+        index += 2
+    # 홀수개 일 경우 남은 하나 처리
+    if index == len(array[i]):
+        result += array[i][-1]
 
 print(result)
--- 4_Sorting/B_3273_남수민.py
+++ 4_Sorting/B_3273_남수민.py
@@ -1,47 +1,30 @@
 # 3273. 두 수의 합
 # 80~84ms
 
-import sys
-input = sys.stdin.readline
+def sol():
+    size = int(input())  # 1 <= n <= 10^6
+    array = sorted(list(map(int, input().split())))  # 1 <= a <= 10^6
+    X = int(input())  # 1 <= X <= 2 * 10^6
 
-size = int(input())  # 1 <= n <= 10^6
-array = sorted(list(map(int, input().split())))  # 1 <= a <= 10^6
-X = int(input())  # 1 <= X <= 2 * 10^6
+    i = 0         # a(i) 탐색 index
+    j = size - 1  # a(j) 탐색 index
+    count = 0     # 조건을 만족하는 쌍 (정답)
 
-idx_a = 0  # array 탐색 index
-pair = []  # a(j)가 될 수 있는 값의 리스트
+    while i < j:
+        sum_ij = array[i] + array[j]
 
-# a(i)를 찾는 과정
-for i in range(size):
-    # a < X/2 일 때, pair에 append
-    if array[i] < X / 2:
-        pair.append(X - array[i])
-    # a > X/2 일 떄, 중단
-    else:
-        idx_a = i
-        break
+        if sum_ij < X:
+            i += 1
 
-# 최선의 시간복잡도를 위해서는 idx 역순 탐색이 유리
-# 할 줄 알았으나 똑같더라 ㅇㅁㅇ,, 왜?
-pair = list(reversed(pair))
+        elif sum_ij > X:
+            j -= 1
 
-count = 0          # 조건을 만족하는 쌍 (정답)
-idx_p = 0          # pair 탐색 index
-len_p = len(pair)  # pair의 길이 (while 조건)
+        else:
+            count += 1
+            i += 1
+            j -= 1
 
-# a(j)를 찾는 과정
-# 모든 pair를 탐색할 때까지 반복
-while idx_p < len_p and idx_a < size:
-    # a < p 일 때, 다음 a에 대하여 비교
-    if array[idx_a] < pair[idx_p]:
-        idx_a += 1
-        continue
+    print(count)
 
-    # a == p 일 때, 카운팅
-    if array[idx_a] == pair[idx_p]:
-        idx_a += 1
-        count += 1
 
-    idx_p += 1  # p_idx 증가
-
-print(count)
+sol()
