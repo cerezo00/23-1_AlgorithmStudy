8d8841addccfa0db87d58ad4774b11abc271ece4
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-02-20 11:35:06 +0900

    Update B_2805_남수민.py

--- 5_BS/B_2805_남수민.py
+++ 5_BS/B_2805_남수민.py
@@ -1,27 +1,30 @@
-# 2417. 정수제곱근
-# 40ms
+# 2805. 나무 자르기
+#
 
-# 접근법
-# 1. 기본 BS 유형
-# 2. 조건 : 같은(X) 같거나 큰(O)
-# 3. 경우의 수
-#    마지막 루프 (low = mid = high)에서 mid가
-#   1) 제곱근 보다 1↓ 작음 -> low가 업데이트(+1)
-#   2) 제곱근 보다 1↓ 큼  -> high가 업데이트(-1)
-# 4. 가능한 솔루션
-#   ㄴ 조건이 "같거나 큰"이므로 +1만큼 업데이트되는 low를 출력한다.
+# collections.Counter 함수
+# - 배열의 원소 별 개수를 Counter 타입으로 리턴
+# ex) Counter([A, B, B, C, C, C])
+#  -> [[A, 1], [B, 2], [C, 3]]
 
-# 사실 어려웠어
+import sys
+from collections import Counter
 
-num = int(input())
+input = sys.stdin.readline
 
+num_tree, req_len = map(int, input().split())
+tree_height_list = Counter(map(int, input().split()))
+
+# BS
 l = 0
-h = num
-
+h = max(tree_height_list.items())[0]
 while l <= h:
-    m = (l + h) // 2  # mid 계산
+    m = (l + h) // 2
 
-    if m ** 2 < num:  # low, high 업데이트
+    sum_len = sum((h-m)*i for h,i in tree_height_list.items() if h>m)
+
+    if sum_len >= req_len:
         l = m + 1
     else:
         h = m - 1
+
+print(h)
